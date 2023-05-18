ff3cdec105fe8703cb903429804547859b878bf6
dillonlee06 <leedillon005@gmail.com>
2023-05-18 13:26:29 -0700

    broke?

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from __init__ import app,db  # Definitions initialization
 from model.jokes import initJokes
 from model.users import initUsers
 from model.players import initPlayers
+from model.leaderboards import initLeaderboards
 
 
 # setup APIs
@@ -15,6 +16,7 @@ from api.covid import covid_api # Blueprint import api definition
 from api.joke import joke_api # Blueprint import api definition
 from api.user import user_api # Blueprint import api definition
 from api.player import player_api
+from api.leaderboard import leaderboard_api
 
 
 # setup App pages
@@ -29,6 +31,7 @@ app.register_blueprint(joke_api) # register api routes
 app.register_blueprint(covid_api) # register api routes
 app.register_blueprint(user_api) # register api routes
 app.register_blueprint(player_api)
+app.register_blueprint(leaderboard_api)
 app.register_blueprint(app_projects) # register app pages
 
 @app.errorhandler(404)  # catch for URL not found
@@ -49,6 +52,7 @@ def activate_job():  # activate these items
     initJokes()
     initUsers()
     initPlayers()
+    initLeaderboards()
 
 # this runs the application on the development server
 if __name__ == "__main__":
